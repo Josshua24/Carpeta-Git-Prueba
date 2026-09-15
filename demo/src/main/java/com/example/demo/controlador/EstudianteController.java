2ef6ef28d01288e2b9d36a0801eb393406618b59
Abner Castillo <abnerjosshuacastillocortes26@gmail.com>
2026-09-15 12:21:13 -0500

    Corrección de sintaxis: se añade punto y coma faltante

--- demo/src/main/java/com/example/demo/controlador/EstudianteController.java
+++ demo/src/main/java/com/example/demo/controlador/EstudianteController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controlador
+package com.example.demo.controlador;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
